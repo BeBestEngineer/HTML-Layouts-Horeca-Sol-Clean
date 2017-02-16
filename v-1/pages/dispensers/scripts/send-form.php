c3e0401777a30e26f8d1cd6ca3d0d7bd5b4c5b9b
Alexandr Ja <mr-spinn@outlook.com>
2017-02-16 10:24:26 +0600

    update send mail script for v-1

--- v-1/pages/dispensers/scripts/send-form.php
+++ v-1/pages/dispensers/scripts/send-form.php
@@ -6,7 +6,7 @@
 
     $text_of_letter = "Прошу отправить на мою почту - ".$email." информацию, о том как я могу получить образец Вашей продукции. С уважением, ".$name;
 
-    if ( mail("385898sw@gmail.com", "Horeca SolClean", $text_of_letter ) ) {
+    if ( mail("LIMANOVSKY@mail.ru, webforward@list.ru", "Horeca SolClean", $text_of_letter ) ) {
         $response_after_send_message['status'] = 'success';
     } else {
         $response_after_send_message['status'] = 'error';
